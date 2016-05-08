88b4943707e6026f7300b50c2b35c2f30227f3d3
Peter Jindra <peterfjindra@gmail.com>
2016-05-08 14:43:44 -0400

    update burst.c. see previous commit

--- burst.c
+++ burst.c
@@ -12,8 +12,41 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+#include <unistd.h>
+
+#define BUFSIZE 4096
 
 int main(int argc, char* argv[]) {
+
+	int infd;
+
+	if(argc > 0){
+		infd = open(argv[1], O_RDONLY);
+	}
+
+	if(infd < 0){
+		perror("Input open error");
+		return 1;
+	}
+
+	char buf[BUFSIZE];
+
+	ssize_t bytesread;
+	while(bytesread = read(infd, buf, BUFSIZE) > 0);
+
+	if(bytesread == 0){
+		puts("done reading the file");
+	}
+
+	else{
+		puts("ERROR");
+	}
+
 
 	return 0;
 }
