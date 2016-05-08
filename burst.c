9a5f1a897d79cfac8e344a39a9916081d1859534
Peter Jindra <peterfjindra@gmail.com>
2016-05-08 13:10:32 -0400

    created burst.c

---
+++ burst.c
@@ -0,0 +1,19 @@
+/*
+
+	burst.c
+
+	A program for splitting up a file.
+
+	Peter Jindra
+	peterfjindra@gmail.com
+	pfj2@zips.uakron.edu
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+
+	return 0;
+}
